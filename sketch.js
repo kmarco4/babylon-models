6b81536bcc9294cce3e83ecfa19c38b16f6f0fe4
kmarco4 <113069202+kmarco4@users.noreply.github.com>
2022-12-09 12:20:58 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,7 @@ var createScene = function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    var seaGull = new meshModel('./seagulf.glb', 0.9)
+   var fish = new meshModel(fish.glb, 0.6);
 
     return scene;
 };
